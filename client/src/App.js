044d4bb8ffb4298376a9b97649bbdb711e408c74
RMarrou <ronaldmarrou@gmail.com>
2020-01-27 02:12:23 -0500

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import React, { Fragment, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Landing, Alert, NavBar } from './components/Layout'
+import { Login, Register } from './components/Auth'
+import Dashboard from './components/Dashboard'
+import { PrivateRoute } from './components/Routing'
+import { Provider } from 'react-redux'
+import store from './store'
+
+import { loadUser } from './actions/auth'
+import setAuthToken from './utils/setAuthToken'
+
+import './App.css'
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token)
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <NavBar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  )
+}
+export default App
---
+++ client/src/components/Layout/index.js
@@ -0,0 +1,4 @@
+export { default as Landing } from './Landing'
+export { default as NavBar } from './Navbar'
+export { default as Alert } from './Alert'
+export { default as Spinner } from './Spinner'
